9d7dd374f1a32e7e0acb6642d2613a8e4263b849
GlincheAutomobiles <charles-louis.girardot@glinche-automobiles.com>
2023-11-20 20:47:45 +0100

    alpinejs is under livewire, hit properties and methods

--- resources/views/livewire/hello-todo.blade.php
+++ resources/views/livewire/hello-todo.blade.php
@@ -1,4 +1,4 @@
-<div class="text-sm">
+<div class="text-sm space-y-8">
     <form wire:submit="addTodo">
         <input wire:model="todo" type="text" name="todo" id="todo" class="text-gray-900 border border-indigo-800 rounded px-1 py-0.5" />
 
@@ -12,4 +12,15 @@
             <li class="italic">No todos ...</li>
         @endforelse
     </ul>
+
+    <div x-data="{ count: 0 }" class="border border-indigo-700 rounded-lg p-5 space-y-3">
+        <p class="text-lg font-semibold">Test AlpineJS</p>
+
+        <div class="space-x-3">
+            <data x-text="count" class="font-mono bg-indigo-50 px-2 py-1 rounded-lg text-indigo-700 font-semibold"></data>
+            <button x-on:click="count++" class="border border-indigo-800 rounded-lg px-2 py-1">+</button>
+        </div>
+
+        <p><span class="font-semibold underline">Current input</span> : <span x-text="$wire.todo" class="text-indigo-700"></span></p>
+    </div>
 </div>
