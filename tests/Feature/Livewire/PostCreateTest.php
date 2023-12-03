5df6de26ea4ea1f0391d4ce589d94373e58cd280
CLGDevWeb <clgdevweb@gmail.com>
2023-12-03 13:27:28 +0100

    (fix) tests

--- tests/Feature/Livewire/PostCreateTest.php
+++ tests/Feature/Livewire/PostCreateTest.php
@@ -2,25 +2,25 @@
 
 namespace Tests\Feature\Livewire;
 
-use App\Livewire\CreatePost;
+use App\Livewire\PostCreate;
 use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 use Tests\TestCase;
 
-class CreatePostTest extends TestCase
+class PostCreateTest extends TestCase
 {
     use RefreshDatabase;
 
     public function test_renders_successfully()
     {
-        Livewire::test(CreatePost::class)
+        Livewire::test(PostCreate::class)
             ->assertStatus(200);
     }
 
     public function test_can_create_post()
     {
-        Livewire::test(CreatePost::class)
+        Livewire::test(PostCreate::class)
             ->set('title', 'Titre cool')
             ->set('content', 'Contenu cool')
             ->set('tags', ['horror', 'fantasy'])
@@ -33,7 +33,7 @@ class CreatePostTest extends TestCase
 
     public function test_title_is_required()
     {
-        Livewire::test(CreatePost::class)
+        Livewire::test(PostCreate::class)
             ->set('title', '')
             ->call('store')
             ->assertHasErrors(['title' => 'required']);
@@ -41,7 +41,7 @@ class CreatePostTest extends TestCase
 
     public function test_title_is_content()
     {
-        Livewire::test(CreatePost::class)
+        Livewire::test(PostCreate::class)
             ->set('content', '')
             ->call('store')
             ->assertHasErrors(['content' => 'required']);
